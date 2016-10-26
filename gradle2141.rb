d9ea8c5d42c572fb21d6ac808238f56f7868a48e
Rick M <rmann@latencyzero.com>
2016-10-25 18:18:26 -0700

    Updated for Gradle v2.14.1

--- gradle2141.rb
+++ gradle2141.rb
@@ -1,7 +1,7 @@
-class Gradle28 < Formula
+class Gradle2141 < Formula
   desc "Gradle build automation tool"
   homepage "https://www.gradle.org/"
-  url "https://downloads.gradle.org/distributions/gradle-2.8-bin.zip"
+  url "https://downloads.gradle.org/distributions/gradle-2.14.1-bin.zip"
   sha256 "a88db9c2f104defdaa8011c58cf6cda6c114298ae3695ecfb8beb30da3a903cb"
 
   bottle :unneeded
